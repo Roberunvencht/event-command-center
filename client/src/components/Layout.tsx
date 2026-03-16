6a0a9eee91765f3eaff7c8a978ee906c3f4238e1
Nick Mours <nickxylanmelloria@gmail.com>
2026-03-16 08:08:24 +0800

    bugfix: client can access admin page

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,17 +1,29 @@
-import { AppSidebar } from './AppSidebar';
-import { Outlet } from 'react-router-dom';
-import { Topbar } from './Topbar';
+import { AppSidebar } from "./AppSidebar";
+import { Outlet, useNavigate } from "react-router-dom";
+import { Topbar } from "./Topbar";
+import { useUserStore } from "@/stores/user";
+import { useEffect } from "react";
 
 export function Layout() {
-	return (
-		<div className='min-h-screen flex w-full'>
-			<AppSidebar />
-			<div className='flex-1 flex flex-col'>
-				<Topbar />
-				<main className='flex-1 p-6'>
-					<Outlet />
-				</main>
-			</div>
-		</div>
-	);
+  const user = useUserStore((state) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user.role === "user") {
+      navigate("/client");
+      return;
+    }
+  }, [user]);
+
+  return (
+    <div className='min-h-screen flex w-full'>
+      <AppSidebar />
+      <div className='flex-1 flex flex-col'>
+        <Topbar />
+        <main className='flex-1 p-6'>
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 }
